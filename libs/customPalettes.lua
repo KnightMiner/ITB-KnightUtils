ac48863ffdafb8c986dde8e208835a5ccda207c8
KnightMiner <knightminer4@gmail.com>
2020-04-08 09:39:33 -0500

    Wrong type of palette

--- libs/customPalettes.lua
+++ libs/customPalettes.lua
@@ -1,51 +1,51 @@
-------------------------------------------------------------------------------------
--- Custom Mech Pallet Library
--- v0.2
--- https://github.com/KnightMiner/ITB-KnightUtils/blob/master/libs/customPallets.lua
-------------------------------------------------------------------------------------
--- Contains helpers to make custom mech pallets
-------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------
+-- Custom Mech Palette Library
+-- v0.4
+-- https://github.com/KnightMiner/ITB-KnightUtils/blob/master/libs/customPalettes.lua
+-------------------------------------------------------------------------------------
+-- Contains helpers to make custom mech palettes
+-------------------------------------------------------------------------------------
 
 -- Current library version, so we can ensure the latest library version is used
-local VERSION = "0.3"
+local VERSION = "0.4"
 
 -- if we have a global that is newer than or the same version as us, use that
 -- if our version is newer or its not yet loaded, load the library
-if CUSTOM_PALLETS == nil or not modApi:isVersion(VERSION, CUSTOM_PALLETS.version) then
+if CUSTOM_PALETTES == nil or not modApi:isVersion(VERSION, CUSTOM_PALETTES.version) then
   -- if we have an older version, update that table with the latest functions
   -- ensures older copies of the library use the latest logic for everything
-  local pallets = CUSTOM_PALLETS or {}
-  CUSTOM_PALLETS = pallets
+  local palettes = CUSTOM_PALETTES or {}
+  CUSTOM_PALETTES = palettes
 
   -- ensure we have needed properties
-  pallets.version = VERSION
+  palettes.version = VERSION
   -- format: map ID -> {name, colors, index}
-  pallets.map = pallets.map or {}
+  palettes.map = palettes.map or {}
     -- format: index -> map ID
-  pallets.indexMap = pallets.indexMap or {}
+  palettes.indexMap = palettes.indexMap or {}
 
   ----------------------
   -- Helper functions --
   ----------------------
 
-  --- Path to animations that use pallets
-  local palletPath = "units/player"
+  --- Path to animations that use palettes
+  local palettePath = "units/player"
 
   --[[--
-    Checks if the given animation object uses pallets
+    Checks if the given animation object uses palettes
 
     @param anim  Animation object
-    @return true if the animation uses pallets
+    @return true if the animation uses palettes
   ]]
-  local function usesPallets(anim)
-    return anim:GetImage():sub(1, #palletPath) == palletPath
+  local function usesPalettes(anim)
+    return anim:GetImage():sub(1, #palettePath) == palettePath
   end
 
   --[[--
-    Updates all animation objects to the updated color count. Needs to be called every time a pallet is added.
-    Needed since increasing the pallet count increases the generated images in an animation
+    Updates all animation objects to the updated color count. Needs to be called every time a palette is added.
+    Needed since increasing the palette count increases the generated images in an animation
 
-    @param added  number of pallets added
+    @param added  number of palettes added
   ]]
   local function updateAnimations(added)
     -- determine the old index we need to update
@@ -60,25 +60,25 @@ if CUSTOM_PALLETS == nil or not modApi:isVersion(VERSION, CUSTOM_PALLETS.version
     -- update other objects that use MechUnit
     for name, anim in pairs(ANIMS) do
       -- images loaded in units/player generate a vertical frame for each unit
-      if type(anim) == "table" and anim.Height ~= nil and anim.Height >= update and anim.Height < count and usesPallets(anim) then
+      if type(anim) == "table" and anim.Height ~= nil and anim.Height >= update and anim.Height < count and usesPalettes(anim) then
         anim.Height = count
       end
     end
   end
 
   --[[--
-    Checks that our library handles mech pallets
+    Checks that our library handles mech palettes
 
-    @return true if this library handles pallets, false otherwise
+    @return true if this library handles palettes, false otherwise
   ]]
-  local function handlesPallets()
-    return GetColorCount == pallets.getCount and GetColorMap == pallets.getColorMap
+  local function handlesPalettes()
+    return GetColorCount == palettes.getCount and GetColorMap == palettes.getColorMap
   end
 
   --[[--
-    Gets the given key from a pallet if the pallet exists
+    Gets the given key from a palette if the palette exists
 
-    @param id       Pallet ID
+    @param id       Palette ID
     @param key      Key to fetch
     @param fallback Optional fallback to use if the map exists, but the key does not
     @return Value of the key, or nil if the map does not exist
@@ -87,13 +87,13 @@ if CUSTOM_PALLETS == nil or not modApi:isVersion(VERSION, CUSTOM_PALLETS.version
     -- IDs loaded from vanilla or libraries besids FURL are numerically indexed
     assert(type(id) == "string", "ID must be a string")
     assert(type(key) == "string", "Key must be a string")
-    -- fetch the pallet if it exists
-    local pallet = pallets.map[id]
-    if pallet == nil then
+    -- fetch the palette if it exists
+    local palette = palettes.map[id]
+    if palette == nil then
       return nil
     end
     -- fallback to ID for the name
-    return pallet[key] or fallback
+    return palette[key] or fallback
   end
 
   -------------
@@ -106,9 +106,9 @@ if CUSTOM_PALLETS == nil or not modApi:isVersion(VERSION, CUSTOM_PALLETS.version
     @param index  Vanilla colormap index
     @return  Colormap ID for the given index
   ]]
-  function pallets.getMapID(index)
+  function palettes.getMapID(index)
     assert(type(index) == "number", "Index must be a number")
-    return pallets.indexMap[index]
+    return palettes.indexMap[index]
   end
 
   --[[--
@@ -118,18 +118,18 @@ if CUSTOM_PALLETS == nil or not modApi:isVersion(VERSION, CUSTOM_PALLETS.version
     @param offset  ImageOffset from the pawn properties
     @return  Colormap ID for the given image offset
   ]]
-  function pallets.getOffsetID(offset)
+  function palettes.getOffsetID(offset)
     assert(type(offset) == "number", "Offset must be a number")
-    return pallets.indexMap[offset+1]
+    return palettes.indexMap[offset+1]
   end
 
   --[[--
-    Gets the name for the given pallet
+    Gets the name for the given palette
 
-    @param id  Pallet ID
-    @return  Pallets name, or nil if the ID does not exist
+    @param id  Palette ID
+    @return  Palettes name, or nil if the ID does not exist
   ]]
-  function pallets.getMapName(id)
+  function palettes.getMapName(id)
     return getIfPresent(id, "name", id)
   end
 
@@ -139,7 +139,7 @@ if CUSTOM_PALLETS == nil or not modApi:isVersion(VERSION, CUSTOM_PALLETS.version
     @param id  Map ID
     @return  Image offset for the given map, or nil if the ID does not exist
   ]]
-  function pallets.getOffset(id)
+  function palettes.getOffset(id)
     local index = getIfPresent(id, "index")
     if index == nil then
       return nil
@@ -152,7 +152,7 @@ if CUSTOM_PALLETS == nil or not modApi:isVersion(VERSION, CUSTOM_PALLETS.version
   -- These two functions are equivelent to the vanilla GetColorMap and GetColorCount respectively
   -- Difference is they are called through this library, and they may be outdated if another
   -- library (such as FURL) overrides the functions
-  -- Calling pallets.migrateHooks() will migrate either vanilla or another library to this library,
+  -- Calling palettes.migrateHooks() will migrate either vanilla or another library to this library,
   -- then ensure this library's functions are used
 
   --[[--
@@ -161,10 +161,10 @@ if CUSTOM_PALLETS == nil or not modApi:isVersion(VERSION, CUSTOM_PALLETS.version
     @param id  Colormap numeric index. Vanilla are 1-9
     @return  Colormap at the given index
   ]]
-  function pallets.getColorMap(index)
+  function palettes.getColorMap(index)
     assert(type(index) == "number", "Index must be a number")
     -- convert the index to an ID, then fetch the coorsponding map
-    local id = pallets.getMapID(index)
+    local id = palettes.getMapID(index)
     if id ~= nil then
       return getIfPresent(id, "colors")
     end
@@ -172,12 +172,12 @@ if CUSTOM_PALLETS == nil or not modApi:isVersion(VERSION, CUSTOM_PALLETS.version
   end
 
   --[[--
-    Gets the number of pallets loaded
+    Gets the number of palettes loaded
 
-    @return  Number of pallets currently loaded
+    @return  Number of palettes currently loaded
   ]]
-  function pallets.getCount()
-    return #pallets.indexMap
+  function palettes.getCount()
+    return #palettes.indexMap
   end
 
   -----------------------
@@ -196,7 +196,7 @@ if CUSTOM_PALLETS == nil or not modApi:isVersion(VERSION, CUSTOM_PALLETS.version
     "Detrius Tan",
     "Vek Purple"
   }
-  --- Internal names for each of the pallets, used for modders
+  --- Internal names for each of the palettes, used for modders
   local vanillaIDs = {
     "RiftWalkers",
     "RustingHulks",
@@ -210,54 +210,54 @@ if CUSTOM_PALLETS == nil or not modApi:isVersion(VERSION, CUSTOM_PALLETS.version
   }
 
   --[[--
-    Migrates missing pallets and overrides the vanilla functions
+    Migrates missing palettes and overrides the vanilla functions
   ]]
-  function pallets.migrateHooks()
-    -- if one of the two vanilla pallet functions is not ours, run migrations
+  function palettes.migrateHooks()
+    -- if one of the two vanilla palette functions is not ours, run migrations
     -- uses the global to ensure we migrate to the latest library version
-    if GetColorCount ~= pallets.getCount or GetColorMap ~= pallets.getColorMap then
-      -- first, clone any pallets we are missing into our array
+    if GetColorCount ~= palettes.getCount or GetColorMap ~= palettes.getColorMap then
+      -- first, clone any palettes we are missing into our array
 
-      local totalPallets = GetColorCount()
-      if totalPallets > pallets.getCount() then
+      local totalPalettes = GetColorCount()
+      if totalPalettes > palettes.getCount() then
         -- first, create a map from indexes to FURL names
         local furlIDs = {}
         if type(FURL_COLORS) == "table" then
           for name, index in pairs(FURL_COLORS) do
-            -- FURL stores imageOffset instead of pallet index
+            -- FURL stores imageOffset instead of palette index
             furlIDs[index+1] = name
           end
         end
 
-        -- migrate any pallets we are missing
-        for index = pallets.getCount()+1, totalPallets do
+        -- migrate any palettes we are missing
+        for index = palettes.getCount()+1, totalPalettes do
           -- first, ensure there is a color map there
           local colors = GetColorMap(index)
           if colors == nil then
             break
           end
 
-          -- use the name from FURL as the ID if present, or fallback to index (vanilla pallets)
+          -- use the name from FURL as the ID if present, or fallback to index (vanilla palettes)
           local id = vanillaIDs[index] or furlIDs[index] or tostring(index)
-          -- create the pallet data
-          pallets.map[id] = {
+          -- create the palette data
+          palettes.map[id] = {
             name = vanillaMapNames[i],
             colors = colors,
             index = index
           }
           -- add the index to the index map, this map may change later
-          pallets.indexMap[index] = id
+          palettes.indexMap[index] = id
         end
       end
 
       -- override the vanilla functions with our copies
-      GetColorMap = pallets.getColorMap
-      GetColorCount = pallets.getCount
+      GetColorMap = palettes.getColorMap
+      GetColorCount = palettes.getCount
     end
   end
 
-  --- List of all key names for indexes in the vanilla pallets structure
-  local PALLET_KEYS = {
+  --- List of all key names for indexes in the vanilla palettes structure
+  local PALETTE_KEYS = {
     "PlateHighlight",
     "PlateLight",
     "PlateMid",
@@ -269,59 +269,59 @@ if CUSTOM_PALLETS == nil or not modApi:isVersion(VERSION, CUSTOM_PALLETS.version
   }
 
   --[[--
-    Adds a new pallet to the game
+    Adds a new palette to the game
 
-    @param ...  Varargs table parameters for pallet data. Contains all the colors from PALLET_KEYS, plus:
-           ID: Unique ID for this pallet
+    @param ...  Varargs table parameters for palette data. Contains all the colors from PALETTE_KEYS, plus:
+           ID: Unique ID for this palette
            Name: Human readible name, if unset defaults to ID
   ]]
-  function pallets.addPallet(...)
-    -- ensure this library is in charge of pallets
-    pallets.migrateHooks()
+  function palettes.addPalette(...)
+    -- ensure this library is in charge of palettes
+    palettes.migrateHooks()
 
-    -- allow passing in multiple pallets at once, more efficient for animation reloading
+    -- allow passing in multiple palettes at once, more efficient for animation reloading
     local datas = {...}
     local added = #datas
     for _, data in ipairs(datas) do
       -- validations
-      assert(type(data) == "table", "Pallet data must be a table")
-      assert(type(data.ID) == "string", "Invalid pallet, missing string ID")
+      assert(type(data) == "table", "Palette data must be a table")
+      assert(type(data.ID) == "string", "Invalid palette, missing string ID")
       assert(data.Name == nil or type(data.Name) == "string", "Name must be a string")
 
-      -- if two mods add a pallet with the same ID, ignore
-      -- allows mods to "share" a pallet
-      if pallets.map[data.ID] ~= nil then
+      -- if two mods add a palette with the same ID, ignore
+      -- allows mods to "share" a palette
+      if palettes.map[data.ID] ~= nil then
         added = added - 1
       else
         -- construct each of the pieces of the color
         local colors = {}
-        for i, key in ipairs(PALLET_KEYS) do
+        for i, key in ipairs(PALETTE_KEYS) do
           if type(data[key]) ~= "table" then
-            error("Invalid pallet, missing key " .. key)
+            error("Invalid palette, missing key " .. key)
           end
           assert(#data[key] == 3, "Color must contain three integers")
           colors[i] = GL_Color(unpack(data[key]))
         end
 
-        -- create the pallet
-        local index = pallets.getCount() + 1
-        pallets.map[data.ID] = {
+        -- create the palette
+        local index = palettes.getCount() + 1
+        palettes.map[data.ID] = {
           name = data.Name,
           colors = colors,
           index = index
         }
-        pallets.indexMap[index] = data.ID
+        palettes.indexMap[index] = data.ID
       end
     end
 
     -- reload animations to update the color count
-    -- only need to reload once for all the pallets
+    -- only need to reload once for all the palettes
     updateAnimations(added)
   end
 end
 
 -- take control of the vanilla functions
-CUSTOM_PALLETS.migrateHooks()
+CUSTOM_PALETTES.migrateHooks()
 
 -- return library, in general the global should not be used outside this script
-return CUSTOM_PALLETS
+return CUSTOM_PALETTES
