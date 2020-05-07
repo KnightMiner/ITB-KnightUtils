6ee6e2515b4aed8cb9e61eda12c89f394fa75e72
KnightMiner <knightminer4@gmail.com>
2020-05-06 22:20:32 -0500

    Better handling for starts belong 4 and make notes column optional

--- libs/personalityCSV.lua
+++ libs/personalityCSV.lua
@@ -1,9 +1,22 @@
 ----------------------------------------------------------------------------------------------
 -- Personality CSV Parser
--- v0.1
+-- v0.2
 -- https://github.com/KnightMiner/ITB-KnightUtils/blob/master/libs/personalityCSV.lua
 ----------------------------------------------------------------------------------------------
 -- Contains helpers to parse the personality CSV files used in 1.1 into the Personality object
+----------------------------------------------------------------------------------------------
+-- Column Format:
+-- * First column is for categories, it is ignored
+-- * Second column contains dialog IDs
+-- * From the third column onwards, any columns with a value in the second row are a pilot
+-- Row Format:
+-- * First row in the pilot's debug name
+-- * Second row is the pilot's
+-- * Third row is the parent pilot, lets you inherit dialog from another pilot
+--   * If the value you pass for the start parameter is 3, no parents supported
+-- * Fourth row is the pilot's in game name. Leave blank for a random name (recruits)
+--   * If the value you pass for start is less than 4, uses first row
+-- Each cell contains the dialog text. Comma separate for multiple options to randomly choose.
 ----------------------------------------------------------------------------------------------
 local ftcsv = require("scripts/personalities/ftcsv")
 local resourcePath = mod_loader.mods[modApi.currentMod].resourcePath
@@ -63,7 +76,8 @@ local personality = {}
   Parses a CSV file and extracts personality data
 
 	@param path   Path to the CSV file, relative to the mod root
-	@param start  First row of dialog data in the CSV file
+	@param start  First row of dialog data in the CSV file.
+	              In vanilla, pilots.csv starts at 8, missions.csv at 3
 ]]
 function personality.load(path, start)
 	-- start the path in the mod folder, appending .csv
@@ -78,23 +92,24 @@ function personality.load(path, start)
 
 	-- first two rows in the CSV are pilot names and IDs
 	local ids = ret[2]
+
 	-- iterate through each pilot, fetching dialog
-	for index = 4, #ids do
+	for index = 3, #ids do
 		local id = ids[index]
 		if id ~= "" then
 			-- if missing from the global, create the personaltiy object for that pilot
 			if Personality[id] == nil then
-				-- third row of CSV lets you clone from another pilot
-				local parent = ret[3][index]
+				-- third row of CSV lets you clone from another pilot, skip if start too early
+				local parent = start > 3 and ret[3][index] or ""
 				if parent == "" or Personality[parent] == nil then
 					Personality[id] = PilotPersonality:new()
 				else
 					Personality[id] = Personality[parent]:new()
 				end
-				-- other data: row 1 is internal names, row 4 display names
+				-- row 1 is debug name, row 4 display names, if start too early use debug name as pilot name
 				-- mainly different for recruits, they have a blank display name to randomly generate
 				Personality[id].Label = ret[1][index]
-				Personality[id].Name = ret[4][index]
+				Personality[id].Name = ret[start > 4 and 4 or 1][index]
 			end
 		end
 	end
@@ -107,11 +122,11 @@ function personality.load(path, start)
 		local trigger = row[2]
 		if trigger ~= "" then
 			-- loop through each column in the row
-			for col = 4, #row do
+			for col = 3, #row do
 				-- skip missing texts
 				local text = row[col]
 				local id = ids[col]
-				if text ~= "" and pilotId ~= "" then
+				if text ~= "" and id ~= "" then
 					-- trim trailing comma
 					if text:sub(#text) == "," then
 						text = text:sub(1,#text-1)
@@ -137,22 +152,6 @@ function personality.load(path, start)
 			end
 		end
 	end
-end
-
---[[--
-  Parses a CEO dialog CSV file and extracts personality data
-	@param path  Path to the CSV file, relative to the mod root
-]]
-function personality.loadCEOs(path)
-	personality.load(path, 3)
-end
-
---[[--
-  Parses a pilot CSV file and extracts personality data
-	@param path  Path to the CSV file, relative to the mod root
-]]
-function personality.loadPilots(path)
-	personality.load(path, 8)
 end
 
 return personality
